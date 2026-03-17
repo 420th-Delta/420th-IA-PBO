485a386609afbf8e0e48f7cfe648618bcfcbd479
Seathre0420 <166345186+Seathre0420@users.noreply.github.com>
2026-03-17 18:13:01 -0400

    fix: register client-side addons to curator modules (#16)
    Co-authored-by: thegamecracks <61257169+thegamecracks@users.noreply.github.com>

--- Apex_framework.terrain/code/config/security.hpp
+++ Apex_framework.terrain/code/config/security.hpp
@@ -229,6 +229,7 @@ class CfgRemoteExec {
 		class BIS_fnc_showNotification {allowedTargets = 1;};
 		class QS_fnc_remoteExec {allowedTargets = 0;};
 		class QS_fnc_remoteExecCmd {allowedTargets = 0;};
+		class TGC_fnc_addCuratorAddons {allowedTargets = 2;};
 		class TGC_fnc_lockDroneByUID {};
 
 		// class RHS_fnc_flashbang_effect {}; // RHSAFRF incorrectly remote executes this
